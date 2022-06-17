a2962ecb9d979b810dcb298921e15518cc10523d
dbaileyc <103800109+dbaileyc@users.noreply.github.com>
2022-06-16 20:54:22 -0400

    updated practice1

--- src/practice1.java
+++ src/practice1.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class practice1 {
+import java.util.Scanner;
+public class practice1 {
+
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c, d;
+        System.out.print(" Enter a : ");
+        a = input.nextInt();
+        System.out.print(" Enter b : ");
+        b = input.nextInt();
+        System.out.print(" Enter c : ");
+        c = input.nextInt();
+        System.out.print(" Enter d : ");
+        d = input.nextInt();
+
+        int Sum = sum(a, b, c, d);
+        System.out.println( a + " + " + b + " + " + c + " + " + d + " " + " = " + Sum);
+
+        int Multiply = multiply(a, b, d);
+        System.out.println( a + " * " + b + " * " + d + " = " + Multiply);
+
+        double Divide = divide(b, d);
+        System.out.println( a + " * " + b + " =" + Divide);
+
+        int Subtract = subtract(b, c);
+        System.out.println( b + " / " + c + " = " + Subtract);
+
+    }
+    public static int sum( int a, int b, int c, int d){
+        int result = a + b + c + d;
+        return result;
+    }
+    public static int multiply(int a, int b, int d){
+        int result = a * b * d;
+        return result;
+    }
+    public static double divide(int b, int d){
+        double result = b / d;
+        return result;
+    }
+    public static int subtract(int b, int c){
+        int result = b - c;
+        return result;
+
+    }
 }
