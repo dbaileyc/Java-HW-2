bb58a0844d1f35fdd69d8be795dc2d1f498dee61
dbaileyc <103800109+dbaileyc@users.noreply.github.com>
2022-06-16 20:56:28 -0400

    updated problem1

--- src/Modified_problem1.java
+++ src/Modified_problem1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class practice1 {
+public class Modified_problem1 {
 
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
